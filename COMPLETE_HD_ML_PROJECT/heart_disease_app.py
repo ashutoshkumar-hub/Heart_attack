c3e0d26ca7041f5697d05cb584cfcd2682aafec2
Ashutosh Kumar <59840130+as416@users.noreply.github.com>
2021-12-07 23:00:11 -0800

    Update heart_disease_app.py

--- COMPLETE_HD_ML_PROJECT/heart_disease_app.py
+++ COMPLETE_HD_ML_PROJECT/heart_disease_app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # Bind home function to URL
 @app.route('/')
 def home():
-    return render_template('Heart Disease Classifier.html')
+    return render_template('main.html')
 
 # Bind predict function to URL
 @app.route('/predict', methods =['POST'])
